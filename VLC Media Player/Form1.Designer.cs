b1b517a9aeee8a8cd4dfa720d95c84f35b4d45ff
Ngo Tu <hoaitu1611@gmail.com>
2021-07-07 22:29:11 +0700

    VLC mediaplayer backup 070721

---
+++ VLC Media Player/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace VLC_Media_Player
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axVLCPlugin21 = new AxAXVLC.AxVLCPlugin2();
+            this.LoadBtn = new System.Windows.Forms.Button();
+            this.playlistVLC = new System.Windows.Forms.ListBox();
+            this.playBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin21)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axVLCPlugin21
+            // 
+            this.axVLCPlugin21.Enabled = true;
+            this.axVLCPlugin21.Location = new System.Drawing.Point(12, 12);
+            this.axVLCPlugin21.Name = "axVLCPlugin21";
+            this.axVLCPlugin21.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axVLCPlugin21.OcxState")));
+            this.axVLCPlugin21.Size = new System.Drawing.Size(504, 381);
+            this.axVLCPlugin21.TabIndex = 0;
+            this.axVLCPlugin21.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.axVLCPlugin21_PreviewKeyDown);
+            // 
+            // LoadBtn
+            // 
+            this.LoadBtn.Location = new System.Drawing.Point(522, 113);
+            this.LoadBtn.Name = "LoadBtn";
+            this.LoadBtn.Size = new System.Drawing.Size(145, 33);
+            this.LoadBtn.TabIndex = 1;
+            this.LoadBtn.Text = "&Load";
+            this.LoadBtn.UseVisualStyleBackColor = true;
+            this.LoadBtn.Click += new System.EventHandler(this.LoadBtn_Click);
+            // 
+            // playlistVLC
+            // 
+            this.playlistVLC.FormattingEnabled = true;
+            this.playlistVLC.Location = new System.Drawing.Point(522, 12);
+            this.playlistVLC.Name = "playlistVLC";
+            this.playlistVLC.Size = new System.Drawing.Size(145, 95);
+            this.playlistVLC.TabIndex = 2;
+            this.playlistVLC.SelectedValueChanged += new System.EventHandler(this.playlistVLC_SelectedValueChanged);
+            // 
+            // playBtn
+            // 
+            this.playBtn.Location = new System.Drawing.Point(522, 152);
+            this.playBtn.Name = "playBtn";
+            this.playBtn.Size = new System.Drawing.Size(145, 33);
+            this.playBtn.TabIndex = 1;
+            this.playBtn.Text = "&Play";
+            this.playBtn.UseVisualStyleBackColor = true;
+            this.playBtn.Click += new System.EventHandler(this.PlayBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 434);
+            this.Controls.Add(this.playlistVLC);
+            this.Controls.Add(this.playBtn);
+            this.Controls.Add(this.LoadBtn);
+            this.Controls.Add(this.axVLCPlugin21);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin21)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxAXVLC.AxVLCPlugin2 axVLCPlugin21;
+        private System.Windows.Forms.Button LoadBtn;
+        private System.Windows.Forms.ListBox playlistVLC;
+        private System.Windows.Forms.Button playBtn;
+    }
+}
+
---
+++ VLC Media Player/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VLC_Media_Player
+{
+    public partial class Form1 : Form, IMessageFilter
+    {
+        private bool enableShortcut = false;
+        private int selectedItem = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                string path = "file:///" + ofd.FileName;
+                addToPlayList(ofd.SafeFileName, path);
+                enableShortcut = true;
+            }
+        }
+
+        private void axVLCPlugin21_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                axVLCPlugin21.playlist.togglePause();
+            }
+        }
+
+        private void PlayBtn_Click(object sender, EventArgs e)
+        {
+            axVLCPlugin21.playlist.playItem(selectedItem);
+        }
+
+        private void playlistVLC_SelectedValueChanged(object sender, EventArgs e)
+        {
+            selectedItem = playlistVLC.SelectedIndex;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Application.AddMessageFilter(this);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.RemoveMessageFilter(this);
+        }
+
+        #region IMessageFilter Members
+
+        private const UInt32 WM_KEYDOWN = 0x0100;
+
+        public bool PreFilterMessage(ref Message m)
+
+        {
+            if (enableShortcut)
+            {
+                if (m.Msg == WM_KEYDOWN)
+                {
+
+                    Keys keyCode = (Keys)(int)m.WParam & Keys.KeyCode;
+                    /* Exit Fullscreen Mode */
+                    if (keyCode == Keys.Escape)
+                    {
+                        this.axVLCPlugin21.video.fullscreen = false;
+                    }
+                    /* Fullscreen Mode */
+                    else if (keyCode == Keys.F)
+                    {
+                        this.axVLCPlugin21.video.toggleFullscreen();
+
+                    }
+                    /* Pause and play */
+                    else if (keyCode == Keys.Space)
+                    {
+                        this.axVLCPlugin21.playlist.togglePause();
+                        var str = TimeSpan.FromMilliseconds(axVLCPlugin21.input.time).ToString(@"hh\:mm\:ss\.fff");
+                    }
+                    /* Next 5s */
+                    else if (keyCode == Keys.Right)
+                    {
+                        this.axVLCPlugin21.input.time += 5000;
+                    }
+                    /* Back 5s */
+                    else if (keyCode == Keys.Left)
+                    {
+                        this.axVLCPlugin21.input.time -= 5000;
+                    }
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        #endregion
+
+        private void addToPlayList(string fileName, string path)
+        {
+            bool enableAdd = true;
+            foreach (string item in playlistVLC.Items)
+            {
+                if (fileName == item)
+                {
+                    MessageBox.Show("The video has been already added.", "Notification",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    enableAdd = false;
+                }
+            }
+            if (enableAdd)
+            {
+                playlistVLC.Items.Add(fileName);
+                axVLCPlugin21.playlist.add(@path, null, null);
+            }
+        }
+    }
+}
